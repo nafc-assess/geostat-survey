65acfc2b37d09c1a8e6c564973843c15a7e25987
Paul Regular <Paul.Regular@dfo-mpo.gc.ca>
2023-05-12 11:13:33 -0230

    First cut exploration of sim_nlf effect of mpa

--- 6_spillover.R
+++ 6_spillover.R
@@ -139,3 +139,55 @@ output2 <- buff_mpa_cell %>%
   bind_rows(cell_wo_mpa_buff)
 
 mapview(output2)
+
+## Sim non linear function -----------------------------------------------------
+## Idea is to increase the probability the fish will occur in the MPA
+
+grid <- make_grid(x_range = c(-150, 150),
+                  y_range = c(-150, 150),
+                  res = c(10, 10),
+                  shelf_depth = 200,
+                  shelf_width = 100,
+                  depth_range = c(0, 1000),
+                  n_div = 1,
+                  strat_breaks = seq(0, 1000, by = 40),
+                  strat_splits = 2,
+                  method = "spline")
+
+## Hack: add mpa column to grid object
+grid_xy <- as.data.frame(grid)
+grid_xy$mpa <- 0
+grid_xy$mpa[grid_xy$y < 50 & grid_xy$y > -120 & grid_xy$x > -65 & grid_xy$x < 100] <- 1
+grid$mpa <- grid_xy$mpa
+
+plot(x ~ y, data = grid_xy, col = grid_xy$mpa)
+
+sim <- list(ages = 1:20, years = 1:20)
+grid_dat <- data.table::data.table(as.data.frame(grid))
+grid_edat <- grid_dat
+i <- rep(seq(nrow(grid_edat)), times = length(sim$ages))
+a <- rep(sim$ages, each = nrow(grid_edat))
+grid_edat <- grid_edat[i]
+grid_edat$age <- a
+i <- rep(seq(nrow(grid_edat)), times = length(sim$years))
+y <- rep(sim$years, each = nrow(grid_edat))
+grid_edat <- grid_edat[i]
+grid_edat$year <- y
+grid_edat <- grid_edat[order(grid_edat$cell, grid_edat$year, grid_edat$age), ] # sort to align with error array
+
+
+depth_par <- sim_nlf(formula = ~ alpha + ifelse(year > 10, (beta * mpa * (year - 10) / 10), 0) - ((depth - mu) ^ 2) / (2 * sigma ^ 2),
+                     coeff = list(alpha = 0, mu = 200, sigma = 70, beta = 0.2))
+
+depth <- depth_par(grid_edat)
+
+grid_edat$depth_effect <- depth
+
+library(plotly)
+grid_edat %>%
+  plot_ly(x = ~depth, y = ~exp(depth_effect), split = ~mpa,
+          frame = ~year) %>%
+  add_lines()
+
+
+
