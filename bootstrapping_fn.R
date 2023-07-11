a1c20fbabd3c3e3672da54f24d4b5a699487f630
Your Name <Semra.Yalcin@dfo-mpo.gc.ca>
2023-07-11 17:18:26 -0230

    data.table edition

--- bootstrapping_fn.R
+++ bootstrapping_fn.R
@@ -1,20 +1,36 @@
 #' Bootstrapping loop functions to calculate CI of design-based estimates
 
+# sumYst <- function(data, i = seq_len(nrow(data))) {
+#   data[i,] |>
+#     ### stratum level
+#     group_by(year, strat, strat_area) |>
+#     summarise(meanYh = mean(n), tow_area = mean(tow_area), .groups = "drop_last") |>
+#     mutate(Nh = strat_area/(tow_area)) |>
+#     group_by(year) |>
+#     mutate(N = sum(Nh), Wh = Nh/N, WhmeanYh = Wh * meanYh)|>
+#     ### year level
+#     summarise(sumYst= mean(N) * sum(WhmeanYh), .groups = "drop_last") |>
+#     pull(sumYst)
+# }
+
 sumYst <- function(data, i = seq_len(nrow(data))) {
-  data[i,] |>
-    ### stratum level
-    group_by(year, strat, strat_area) |>
-    summarise(meanYh = mean(n), tow_area = mean(tow_area), .groups = "drop_last") |>
-    mutate(Nh = strat_area/(tow_area)) |>
-    group_by(year) |>
-    mutate(N = sum(Nh), Wh = Nh/N, WhmeanYh = Wh * meanYh)|>
-    ### year level
-    summarise(sumYst= mean(N) * sum(WhmeanYh), .groups = "drop_last") |>
-    pull(sumYst)
+  dt <- data.table(data)
+  dt <- dt[i,]
+
+  # stratum level
+  dt <- dt[, .(meanYh = mean(n), tow_area = mean(tow_area)), by = .(year, strat, strat_area)][
+    , Nh := strat_area / tow_area][
+      , .(N = sum(Nh), Wh = Nh / sum(Nh), WhmeanYh = Nh / sum(Nh) * meanYh), by = year]
+
+  # year level
+  sumYst <- dt[, .(sumYst = mean(N) * sum(WhmeanYh)), by = year][
+    , sumYst]
+
+  return(sumYst)
 }
 
 boot_one_year <- function(data, reps) {
-  b <- boot::boot(data, statistic = sumYst, strata = data$strat, R = reps, parallel = "multicore", ncpus=6)
+  b <- boot::boot(data, statistic = sumYst, strata = data$strat, R = reps)
   suppressWarnings(bci <- boot::boot.ci(b, type = "bca"))
   mean_boot <- mean(b$t)
   boot <- data.table(
@@ -27,7 +43,6 @@ boot_one_year <- function(data, reps) {
     cv = sd(b$t) / mean_boot,
     type = "Bootstrapped")
   return(boot)
-  gc()
 }
 
 boot_wrapper <- function(data, reps) {
@@ -37,5 +52,7 @@ boot_wrapper <- function(data, reps) {
   out$year <- as.numeric(out$year)
   return(out)
 }
+
+
 
 
