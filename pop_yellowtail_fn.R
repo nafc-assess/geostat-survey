30a0dbbe1bb02ddc591109a654b214535f3d74aa
Your Name <Semra.Yalcin@dfo-mpo.gc.ca>
2023-07-13 14:37:13 -0230

    Fixing seeds

--- pop_yellowtail_fn.R
+++ pop_yellowtail_fn.R
@@ -5,32 +5,32 @@
 #'
 population_yellowtail <- function(iter, n_sims) {
   set.seed(iter * 1)
-  pop <- SimSurvey::sim_abundance(ages = 1:10,
-                                  years = 1:20,
+  pop <- sim_abundance(ages = 1:10,
+                       years = 1:20,
 
-                                  Z = sim_Z(log_mean = log(0.5),
-                                            log_sd = 0.2,
-                                            phi_age = 0.3,
-                                            phi_year = 0.4),
+                       Z = sim_Z(log_mean = log(0.5),
+                                 log_sd = 0.2,
+                                 phi_age = 0.3,
+                                 phi_year = 0.4),
 
-                                  R = sim_R(log_mean = log(8e+09),
-                                            log_sd = 0.2,
-                                            random_walk = FALSE),
+                       R = sim_R(log_mean = log(8e+09),
+                                 log_sd = 0.2,
+                                 random_walk = FALSE),
 
-                                  N0 = sim_N0(N0 = "exp", plot = FALSE),
-                                  growth = sim_vonB(Linf = 56, L0 = 0,
-                                                    K = 0.13, log_sd = 0.12,
-                                                    length_group = 2, digits = 0)) |>
-    sim_distribution(make_grid(x_range = c(-150, 150),
-                               y_range = c(-150, 150),
-                               res = c(10, 10),
-                               shelf_depth = 200,
-                               shelf_width = 100,
-                               depth_range = c(0, 1000),
-                               n_div = 1,
-                               strat_breaks = seq(0, 1000, by = 40),
-                               strat_splits = 2,
-                               method = "spline"),
+                       N0 = sim_N0(N0 = "exp", plot = FALSE),
+                       growth = sim_vonB(Linf = 56, L0 = 0,
+                                         K = 0.13, log_sd = 0.12,
+                                         length_group = 2, digits = 0)) |>
+    sim_distribution(grid = make_grid(x_range = c(-150, 150),
+                                      y_range = c(-150, 150),
+                                      res = c(10, 10),
+                                      shelf_depth = 200,
+                                      shelf_width = 100,
+                                      depth_range = c(0, 1000),
+                                      n_div = 1,
+                                      strat_breaks = seq(0, 1000, by = 40),
+                                      strat_splits = 2,
+                                      method = "spline"),
 
                      ays_covar = sim_ays_covar(sd = 2.8,
                                                range = 500,
@@ -41,14 +41,14 @@ population_yellowtail <- function(iter, n_sims) {
                      depth_par = sim_parabola(mu = log(90),
                                               sigma = 0.3,
                                               log_space = TRUE))
-  #set.seed(35)
+  set.seed(iter * 35)
   survey <- SimSurvey::sim_survey(pop, n_sims = n_sims,
-                          q = sim_logistic(k = 1.6, x0 = 5.5, plot = FALSE),
-                          trawl_dim = c(3, 0.02),
-                          resample_cells = FALSE,
-                          binom_error = TRUE,
-                          min_sets = 2,
-                          set_den = 2/1000)
+                                  q = sim_logistic(k = 1.6, x0 = 5.5, plot = FALSE),
+                                  trawl_dim = c(3, 0.02),
+                                  resample_cells = FALSE,
+                                  binom_error = TRUE,
+                                  min_sets = 2,
+                                  set_den = 2/1000)
 }
 
 #### Recovery Scenario
@@ -101,7 +101,7 @@ population_yellowtail_recovery <- function(iter, n_sims) {
                                               phi_year = 0.8,
                                               group_ages = 5:9),
                                 depth_par = depth_mpa_par_recovery)
-  #set.seed(35)
+  set.seed(35)
   survey <- SimSurvey::sim_survey(pop, n_sims = n_sims,
                                   q = sim_logistic(k = 1.6, x0 = 5.5, plot = FALSE),
                                   trawl_dim = c(3, 0.02),
@@ -162,14 +162,14 @@ population_yellowtail_spillover <- function(iter, n_sims) {
                                               phi_year = 0.8,
                                               group_ages = 5:9),
                                 depth_par = depth_mpa_par)
-    #set.seed(35)
-    survey <- SimSurvey::sim_survey(pop, n_sims = n_sims,
-                          q = sim_logistic(k = 1.6, x0 = 5.5, plot = FALSE),
-                          trawl_dim = c(3, 0.02),
-                          resample_cells = FALSE,
-                          binom_error = TRUE,
-                          min_sets = 2,
-                          set_den = 2/1000)
+  set.seed(35)
+  survey <- SimSurvey::sim_survey(pop, n_sims = n_sims,
+                                  q = sim_logistic(k = 1.6, x0 = 5.5, plot = FALSE),
+                                  trawl_dim = c(3, 0.02),
+                                  resample_cells = FALSE,
+                                  binom_error = TRUE,
+                                  min_sets = 2,
+                                  set_den = 2/1000)
 }
 
 
